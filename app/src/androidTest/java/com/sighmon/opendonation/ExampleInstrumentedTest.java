9a96e5c76a200ed6df9f10030b797528e7100c03
sighmon <simon.loffler@gmail.com>
2018-04-12 23:38:50 +0930

    Example test had old package name.

--- app/src/androidTest/java/com/sighmon/opendonation/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sighmon/opendonation/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("sighmon.com.opendonation", appContext.getPackageName());
+        assertEquals("com.sighmon.opendonation", appContext.getPackageName());
     }
 }
